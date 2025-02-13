55269c38b180e513a89426d3570e9633b10fe3b2
Ahmad Mubarak <ahmdmubarak01@gmail.com>
2025-02-13 12:59:26 +0700

    menyelesaikan kodingan js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 // ini file javascript
+
+function validateForm() {
+    // console.log('Berhasil di eksekusi!');
+
+    let input = document.getElementById('main-input');
+    console.log(input.value);
+
+    if (input.value == '') {
+        alert('Tolong isi inputan!');
+    } else {
+        // proses konversi
+        let calc = convertToCelcius(input.value);
+
+        // tampilkan hasil
+        document.getElementById('main-result').value = calc;
+        document.getElementById('cara-konversi').value = 'Diket: C= ' + input.value + ' F...';
+        console.log('Berhasil di eksekusi!');
+    }
+}
+
+// rumus konversi
+let convertToCelcius = (input) => {
+    return input * 2;
+}
+
